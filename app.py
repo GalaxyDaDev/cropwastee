f25086265550bc48c57458972f92677742ea5a04
GalaxyDaDev <galaxyboyplaysroblox@gmail.com>
2024-07-27 21:50:58 +0530

    beggin

--- app.py
+++ app.py
@@ -7,25 +7,21 @@ import requests
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Enable CORS
 
-# GitHub repository details
-github_release_url = 'https://github.com/GalaxyDaDev/cropwastee/releases/download/ai/husk_model.pth'
+# Model setup
+model_url = 'https://github.com/GalaxyDaDev/cropwastee/releases/download/ai/husk_model.pth'
 model_path = 'husk_model.pth'
 
-def download_model_from_github():
+def download_model():
     if not os.path.isfile(model_path):
         print('Downloading model from GitHub...')
-        response = requests.get(github_release_url)
-        if response.status_code == 200:
-            with open(model_path, 'wb') as file:
-                file.write(response.content)
-            print('Model downloaded successfully.')
-        else:
-            print('Failed to download model:', response.status_code, response.text)
+        response = requests.get(model_url)
+        with open(model_path, 'wb') as file:
+            file.write(response.content)
+        print('Model downloaded successfully.')
 
-# Load the model
-download_model_from_github()
+download_model()
 model = models.resnet18(weights='DEFAULT')
 num_classes = 8
 in_features = model.fc.in_features
@@ -33,7 +29,6 @@ model.fc = torch.nn.Linear(in_features, num_classes)
 model.load_state_dict(torch.load(model_path))
 model.eval()
 
-# Define preprocessing transformation
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -41,8 +36,11 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-# Define the class labels
 class_labels = ['Chickpea Husk', 'Corn Husk', 'Field Pea Husk', 'Grass Pea Husk', 'Lentil Husk', 'Rice Husk', 'Wheat Husk', 'Soybean Husk']
+
+@app.route('/')
+def index():
+    return 'Flask API is running. Use /predict to make predictions.'
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -52,11 +50,9 @@ def predict():
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
     
-    # Load and preprocess the image
     image = Image.open(file.stream).convert('RGB')
     image = preprocess(image).unsqueeze(0)
     
-    # Predict
     with torch.no_grad():
         outputs = model(image)
         _, predicted = torch.max(outputs, 1)
